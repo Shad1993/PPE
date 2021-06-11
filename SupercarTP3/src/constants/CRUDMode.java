65b9b1c408812f612fa54b7adc08001b8c331b6b
Reshad Oderuth <reshado@student.mccibs.ac.mu>
2021-06-12 01:00:45 +0400

    PPE3V1.0

---
+++ SupercarTP3/src/constants/CRUDMode.java
@@ -0,0 +1,17 @@
+package constants;
+
+/*
+ * @author Sam Ong
+ * 
+ * Only {MAIN.java see link} needs this.
+ * 
+ */
+
+public enum CRUDMode {
+	ADD, 
+	UPDATE, 
+	DELETE, 
+	QUIT, 
+	LISTALLSTUDENTS,
+	INVALID
+}
